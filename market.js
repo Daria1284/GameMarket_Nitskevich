4a347480cb431a177f48ab84ad52fd30700d789f
Daria1284 <158023033+Daria1284@users.noreply.github.com>
2024-05-08 09:50:16 +0300

    1

--- market.js
+++ market.js
@@ -1,11 +1,20 @@
-//Перевірка підключеного файлу скриптів
-console.log('Перевірка підключеного файлу скриптів market.js')
-// Отримання елементу з ідентифікатором items
-let itemsDiv =document.getElementById("items"); 
+// //Перевірка підключеного файлу скриптів
+// console.log('Перевірка підключеного файлу скриптів market.js')
+// // Отримання елементу з ідентифікатором items
+// let itemsDiv =document.getElementById("items"); 
+//Визначення масиву товарів
+let itemsArray = [
+    'Газоноосарка 43',
+    'Електричний тример 110',
+    'Електрична газонокосарка 32',
+    'Акумуляторний оприскувач 12 N'
+]
+//Отримання елементу з індифекатором items
+let itemsDiv = document.getElementById("items");
 //Перевірка існування знайденого блоку 
 if (itemsDiv) { 
-    //Вивід знайденого елементу 
-    console.log(itemsDiv) 
+    //Виведення елементів масиву
+    console.log(item) 
     //Додавання відформатованого HTML коду в блок 
     // itemsDiv.innerHTML += '<div class = "item"></div>' 
     // itemsDiv.innerHTML += '<div class = "item"></div>' 
@@ -18,3 +27,24 @@ if (itemsDiv) {
     //Вивід повідомлення про не знайдений блок 
     console.log('Блок товарів не знайдено')
 } 
+
+//Виведення елементів масиву
+itemsArray.forEach((item)=>{
+    console.log(item)
+})
+// //Виведення елементів відсортованого масиву
+// itemsArray.sort().forEach((item,index) =>{
+//     console.log(index + '-й елемент:',item)
+// })
+// //Сортування масиву
+// itemsArray = itemsArray.sort()
+// // Виведення в консоль масиву
+// console.log(itemsArray)
+// //Виведення в консоль елементів масиву
+// for(let i = 0; i<itemsArray.length; i++){
+//     console.log(itemsArray[i])
+// }
+// //Виведення в коносль номерів та значень елементів масиву
+// for(let i = 0; i < itemsArray.length; i++){
+//     console.log(i +'-й елемент: ', itemsArray[i])
+// }
